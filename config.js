105122b3a70b0dd2546a2e3f5874b7cad8c24430
rasinth <nimeshbandara9999@gmail.com>
2024-08-13 14:26:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94783390181', 'me', true],
+  ['94783390181', 'Rasintha Bandara', true], 
   [''] 
 ] //Number of owners
 
